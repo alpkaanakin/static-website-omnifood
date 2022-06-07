1dd639c58ba5733347515e101c2e09a6d2fa237f
mist-magister <alpkaganakin@gmail.com>
2022-06-07 18:46:10 +0300

    create website

---
+++ main.js
@@ -0,0 +1,54 @@
+const yearEl = document.querySelector(".current-year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+const mobileNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+mobileNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    rootMargin: "-120px",
+    threshold: 0,
+  }
+);
+obs.observe(sectionHeroEl);
